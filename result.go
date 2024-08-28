4f3de8c3e8be0e391c7b73d3e0b99dba9933ea5d
Chris Lesiw <chris@chrislesiw.com>
2024-08-27 21:19:47 -0400

    Add TODO on CmdResult to clarify its incompleteness

--- result.go
+++ result.go
@@ -3,6 +3,7 @@ package cmdio
 import "io"
 
 // CmdResult describes the results of a completed read from an [io.Reader].
+// TODO: Incorporate stderr.
 type CmdResult struct {
 	Cmd    io.Reader
 	Output string
